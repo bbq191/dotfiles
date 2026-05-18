be23e2ce96af07f2755db6d255c1fdfbd155664f
afu <377810+bbq191@users.noreply.github.com>
2026-05-18 18:53:20 +0800

    feat: add obsync function for Obsidian vault git sync

---
+++ home/.config/fish/functions/obsync.fish
@@ -0,0 +1,7 @@
+function obsync --description "Sync Obsidian vault via git"
+    set -l vault ~/Documents/ikate
+    git -C $vault pull || return 1
+    git -C $vault add -A
+    git -C $vault diff --cached --quiet; or git -C $vault commit -m "sync "(date +%H:%M)
+    git -C $vault push
+end
