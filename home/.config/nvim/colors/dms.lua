32d30ccc48b92aa11a89e3c654ef73b330381028
afu <377810+bbq191@users.noreply.github.com>
2026-09-06 19:52:57 +0800

    chore: matugen 主题重生成

--- home/.config/nvim/colors/dms.lua
+++ home/.config/nvim/colors/dms.lua
@@ -86,9 +86,9 @@ end
 
 if not base46.theme_tables[theme_name] or base46.theme_tables[theme_name].type ~= vim.o.background then
 	local builtin = vim.deepcopy(assert(base46.get_builtin_theme(theme_base)))
-	local harmonized = base46.theme_harmonize(builtin, "#d53349", harmony)
+	local harmonized = base46.theme_harmonize(builtin, "#549ab4", harmony)
 	if settings.matugenTemplateNeovimSetBackground ~= false then
-		harmonized = base46.theme_set_bg(harmonized, "#fff8f7")
+		harmonized = base46.theme_set_bg(harmonized, "#101415")
 	end
 
 	base46.theme_tables[theme_name] = harmonized
